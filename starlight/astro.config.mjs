8f7633caef76fbd262b7dea6f8fd1b74f40b4574
Mario Wegmann <mario.wegmann@web.de>
2025-03-02 17:33:07 +0100

    Creation of Astro project with Starlight template

---
+++ starlight/astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Kontoflow Dokumentation',
+			social: {
+				github: 'https://github.com/Kontoflow/kontoflow-doku',
+			},
+			sidebar: [
+				{
+					label: 'Installation',
+					autogenerate: { directory: 'installation' },
+				},
+				{
+					label: 'Bedienung',
+					autogenerate: { directory: 'bedienung' },
+				},
+				{
+					label: 'Changelog',
+					autogenerate: { directory: 'changelog' },
+				}
+			],
+		}),
+	],
+});
